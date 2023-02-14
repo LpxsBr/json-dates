83d791020dca0c691de77cc416e00aa80f60021b
LpxsBr <anselmolopes.an@gmail.com>
2023-02-14 10:35:21 -0300

    feat: add number of the days of week

--- src/data.js
+++ src/data.js
@@ -1,24 +1,31 @@
 const week =[
     {
+        num: 1,
+        pt: 'Domingo'
+    },
+    {
+        num: 2,
         pt: 'Segunda'
     },
     {
+        num: 3,
         pt: 'Terça'
     },
     {
+        num: 4,
         pt: 'Quarta'
     },
     {
+        num: 5,
         pt: 'Quinta'
     },
     {
+        num: 6,
         pt: 'Sexta'
     },
     {
+        num: 7,
         pt: 'Sábado'
-    },
-    {
-        pt: 'Domingo'
     }
 ]
 
